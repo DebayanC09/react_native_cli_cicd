96cd52aa82846e0d202581e237ae844a800ced0e
Debayan Chowdhury <debayan301993@gmail.com>
2025-02-22 19:57:22 +0530

    add environment configuration support and update app display to show current environment

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import Config from 'react-native-config';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.screen}>
       <View style={styles.view}>
-        <Text>App</Text>
+        <Text>{`App ${Config.APP_ENV}`}</Text>
       </View>
     </SafeAreaView>
   );
